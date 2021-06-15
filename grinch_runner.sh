7535123222d740a344b0f1481188f51664065e1b
aineniamh <aine.otoole@ed.ac.uk>
2021-06-15 10:52:05 +0100

    updating runner

--- grinch_runner.sh
+++ grinch_runner.sh
@@ -12,28 +12,28 @@ python setup.py install
 
 grinch -t 2 -i /raid/shared/grinch/grinch/data/config.yaml --outdir "/raid/shared/$TODAY" --output-prefix global_report
 
-cd /home/shared/lineages-website && git pull
+cd /raid/shared/lineages-website && git pull
 
-python /raid/shared/grinch/grinch/scripts/update_website.py --website-dir /home/shared/lineages-website -m /raid/shared/$TODAY/2/lineages.metadata.csv -d /raid/shared/pango-designation/lineages.csv -n /raid/shared/pango-designation/lineage_notes.txt -o /home/shared/lineages-website/_data/lineage_data.json
+python /raid/shared/grinch/grinch/scripts/update_website.py --website-dir /raid/shared/lineages-website -m /raid/shared/$TODAY/2/lineages.metadata.csv -d /raid/shared/pango-designation/lineages.csv -n /raid/shared/pango-designation/lineage_notes.txt -o /raid/shared/lineages-website/_data/lineage_data.json
 
-cp /raid/shared/$TODAY/report/global_report_B.1.1.7.md /home/shared/lineages-website/global_report_B.1.1.7.md
-cp /raid/shared/$TODAY/report/global_report_B.1.351.md /home/shared/lineages-website/global_report_B.1.351.md
-cp /raid/shared/$TODAY/report/global_report_P.1.md /home/shared/lineages-website/global_report_P.1.md
-cp /raid/shared/$TODAY/report/grinch_data.json /home/shared/lineages-website/_data/grinch_data.json
+cp /raid/shared/$TODAY/report/global_report_B.1.1.7.md /raid/shared/lineages-website/global_report_B.1.1.7.md
+cp /raid/shared/$TODAY/report/global_report_B.1.351.md /raid/shared/lineages-website/global_report_B.1.351.md
+cp /raid/shared/$TODAY/report/global_report_P.1.md /raid/shared/lineages-website/global_report_P.1.md
+cp /raid/shared/$TODAY/report/grinch_data.json /raid/shared/lineages-website/_data/grinch_data.json
 
-cp /raid/shared/$TODAY/figures/*.svg /home/shared/lineages-website/assets/images/
+cp /raid/shared/$TODAY/figures/*.svg /raid/shared/lineages-website/assets/images/
 
-cp /raid/shared/$TODAY/2/lineages.metadata.csv /home/shared/lineages-website/_data/lineages.metadata.csv
+cp /raid/shared/$TODAY/2/lineages.metadata.csv /raid/shared/lineages-website/_data/lineages.metadata.csv
 
-cd /home/shared/lineages-website && git pull
+cd /raid/shared/lineages-website && git pull
 
-git add /home/shared/lineages-website/lineages.md
-git add /home/shared/lineages-website/lineages/*.md
-git add /home/shared/lineages-website/global_report_*.md
-git add /home/shared/lineages-website/_data/lineages.metadata.csv
-git add /home/shared/lineages-website/_data/lineage_data.json
-git add /home/shared/lineages-website/_data/grinch_data.json
-git add /home/shared/lineages-website/assets/images/*.svg
+git add /raid/shared/lineages-website/lineages.md
+git add /raid/shared/lineages-website/lineages/*.md
+git add /raid/shared/lineages-website/global_report_*.md
+git add /raid/shared/lineages-website/_data/lineages.metadata.csv
+git add /raid/shared/lineages-website/_data/lineage_data.json
+git add /raid/shared/lineages-website/_data/grinch_data.json
+git add /raid/shared/lineages-website/assets/images/*.svg
 
 git commit -m "updating new variant report $TODAY"
 git push
