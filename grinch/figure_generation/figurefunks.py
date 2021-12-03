5902d8a8a22afb12962b3d698916e36f2c96dd41
aineniamh <aine.otoole@ed.ac.uk>
2021-12-03 09:01:57 +0000

    svg cut off now too

--- grinch/figure_generation/figurefunks.py
+++ grinch/figure_generation/figurefunks.py
@@ -90,37 +90,53 @@ def make_dataframe(metadata, conversion_dict2, omitted, lineage_of_interest, fig
     with open(metadata) as f:
         data = csv.DictReader(f)
         
-        cut_off = dt.datetime.strptime("2020-09-01", "%Y-%m-%d").date()
-        for seq in data:
-            try:
-                sample_date = dt.datetime.strptime(seq["sample_date"], "%Y-%m-%d").date()
-                if sample_date < cut_off:
-                    pass
-                else:
-                    if seq["lineage"] == lineage_of_interest:
-                        seq_country = seq["country"].upper().replace(" ","_")
-                        if seq_country == "CARIBBEAN":
-                            seq_country = seq["sequence_name"].split("/")[0].upper().replace(" ","_")
-                            
-                        if seq_country in conversion_dict2:
-                            new_country = conversion_dict2[seq_country]
-                        else:
-                            if seq_country not in omitted:
-                                new_country = seq_country
+        cut_off = datetime.strptime("2020-09-01", "%Y-%m-%d").date()
+                
+        lineage = row["lineage"]
+
+        if lineage == "B.1.1.7": 
+            cut_off = datetime.strptime("2020-09-01", "%Y-%m-%d").date()
+        elif lineage == "B.1.351":
+            cut_off = datetime.strptime("2020-09-01", "%Y-%m-%d").date()
+        elif lineage == "P.1":
+            cut_off = datetime.strptime("2020-09-01", "%Y-%m-%d").date()
+        elif lineage == "B.1.617.2":
+            cut_off = datetime.strptime("2021-03-01", "%Y-%m-%d").date()
+        elif lineage == "B.1.1.529":
+            cut_off = datetime.strptime("2021-09-01", "%Y-%m-%d").date()
+        if d < cut_off: 
+            pass
+        else:
+            for seq in data:
+                try:
+                    sample_date = dt.datetime.strptime(seq["sample_date"], "%Y-%m-%d").date()
+                    if sample_date < cut_off:
+                        pass
+                    else:
+                        if seq["lineage"] == lineage_of_interest:
+                            seq_country = seq["country"].upper().replace(" ","_")
+                            if seq_country == "CARIBBEAN":
+                                seq_country = seq["sequence_name"].split("/")[0].upper().replace(" ","_")
+                                
+                            if seq_country in conversion_dict2:
+                                new_country = conversion_dict2[seq_country]
                             else:
-                                new_country = ""
-                        
-                        if new_country not in countries and new_country != "":
-                            absent_countries.add(new_country)
-                            pass
-                        elif new_country != "":
-                            try:
-                                locations_to_dates[new_country].append(dt.datetime.strptime(seq["sample_date"], "%Y-%m-%d").date())
-                            except:
+                                if seq_country not in omitted:
+                                    new_country = seq_country
+                                else:
+                                    new_country = ""
+                            
+                            if new_country not in countries and new_country != "":
+                                absent_countries.add(new_country)
                                 pass
-                        country_to_new_country[seq_country] = new_country
-            except:
-                pass
+                            elif new_country != "":
+                                try:
+                                    locations_to_dates[new_country].append(dt.datetime.strptime(seq["sample_date"], "%Y-%m-%d").date())
+                                except:
+                                    pass
+                            country_to_new_country[seq_country] = new_country
+                except:
+                    pass
     
 
     loc_to_earliest_date = {}
